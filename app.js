f2f3fac62a0eaac2ecb49437f63dfa6d7ed55b1d
Yomi-Dev <102952655+Yomi-Dev@users.noreply.github.com>
2022-05-18 16:42:58 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+//  Change the NavBar styles OnScroll
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
+})
+
+// Show Or Hide Faq Answer
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        // change Icon
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'fa-solid fa-plus') {
+            icon.className = `fa-solid fa-minus`;
+        } else {
+            icon.className = 'fa-solid fa-plus';
+        }
+    })
+})
+
+
+
+//   show/hide nav menu
+const menu = document.querySelector(".nav_menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+// Create EventListeners
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav);
